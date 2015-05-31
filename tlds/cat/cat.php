1996671fbba19db4e141eb432ea30dae9f50907a
Pedro Frejo <pedro.frejo@arpasystem.com>
2015-05-31 02:02:00 +0200

    Added TLD: cat

---
+++ tlds/cat/cat.php
@@ -0,0 +1,19 @@
+<?php
+
+class cat extends Request {	
+	public function registerDomain($params=false) {
+		$params = $this->setParams($params);
+		$ascioParams = $this->mapToOrder($params,"Register_Domain");
+		
+		/* Use Post-Validation System (See TLD kit) */
+		$ascioParams["order"]["Domain"]["AuthInfo"] = "DEFERRED_VAL";
+		$ascioParams["order"]["Domain"]["DomainPurpose"] = $params["additionalfields"]["Intended Use"];
+
+		$result = $this->request("CreateOrder",$ascioParams);
+		if (!$result) {
+			$this->setWhmcsStatus($domainName,"Pending","Register_Domain");
+		}
+		return $result;
+	}
+}
+?>
